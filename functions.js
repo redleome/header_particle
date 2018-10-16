4f21cd2f5b11ae35717f3cce9d70c811c549aeaa
redleome <redleome@gmail.com>
2018-10-16 11:15:30 +0200

    version 1

--- functions.js
+++ functions.js
@@ -1,1 +1,18 @@
-alert("test");
+window.onload= function(){
+  var placeholder = document.getElementById("placeholder");
+  var content= [".net","java","techpros"];
+  var count = 0;
+  var frames= 10;
+  var current_frame=0;
+
+  setInterval(function(){
+    console.log(content.length);
+    if(count < content.length) {
+
+      placeholder.innerHTML = content[count];
+      // placeholder.classList.add('switch');
+      // placeholder.style.opacity ="1";
+      count = count + 1;}
+    else {  count = 0; }
+  }, 1000);
+}
